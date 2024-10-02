e8d1858905a5c968e525c1de871a72e0a83c5f2a
zitchu <paradox0w@hotmail.com>
2024-10-02 13:11:07 -0300

    alinhamentos stk

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,41 +5,41 @@ function Sidebar() {
   return (
     <div>
       <nav>
-        <ul className="cinzel-regular fixed z-10 flex justify-around w-full silver-bg backdrop-blur-sm h-[50px] p-1 ">
+        <ul className="cinzel-regular fixed z-10 flex flex-wrap justify-around w-full silver-bg backdrop-blur-sm h-[100px] md:h-[50px] p-1 ">
           <div className="absolute rounded-full silver-prego shadow-lg w-2 h-2 left-0 m-1 top-0" />
           <div className="absolute rounded-full silver-prego shadow-lg w-2 h-2 left-0 m-1 bottom-0" />
           <li>
-            <button className="rounded-lg gold-button hover:shadow-md text-sm md:text-lg w-auto p-2 h-full transition-all duration-1000 ease-in-out">
+            <button className="rounded-lg gold-button h-[70%] hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full transition-all duration-1000 ease-in-out">
               <Link to="/">Home</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg silver-button  transition-all duration-700 hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg silver-button h-[70%]  transition-all duration-700 hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/phandalim">Phandalim</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg silver-button   hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg silver-button h-[70%]   hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/stk">STK</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg silver-button   hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg silver-button h-[70%]   hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/tomb">Tomb</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg silver-button   hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg silver-button h-[70%]   hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/dualidade">Dualidade</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg silver-button   hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg silver-button h-[70%]   hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/tales">Fairy Tales</Link>
             </button>
           </li>
           <li>
-            <button className="rounded-lg bronze-button   hover:shadow-md text-sm md:text-lg w-auto p-2 h-full">
+            <button className="rounded-lg bronze-button h-[70%]   hover:shadow-md text-sm md:text-lg w-auto p-2 md:h-full">
               <Link to="/misc">Variados</Link>
             </button>
           </li>
--- src/pages/STK.js
+++ src/pages/STK.js
@@ -13,57 +13,154 @@ import fire from "../components/img/stk/fire.png";
 import fire2 from "../components/img/stk/fire2.png";
 import frost from "../components/img/stk/frost.png";
 import frost2 from "../components/img/stk/frost2.png";
+import harshnag from "../components/img/stk/harshnag.png";
 import hecaton from "../components/img/stk/hecaton.png";
 import hill from "../components/img/stk/hill.png";
 import hill2 from "../components/img/stk/hill2.png";
 import lymrith from "../components/img/stk/lymrith.png";
 import stone from "../components/img/stk/stone.png";
+import stone2 from "../components/img/stk/stone2.png";
 import storm from "../components/img/stk/storm.png";
 import storm2 from "../components/img/stk/storm2.png";
 import zephyros from "../components/img/stk/zephyros.png";
 
 function STK() {
   const images = [
-    dreeco2,
-    climber2,
-    loy2,
-    verdan2,
-    sylas2,
-    leon2,
-    joia2,
-    mk2,
-    cloud,
-    cloud2,
-    fire,
-    fire2,
-    frost,
-    frost2,
-    hecaton,
-    hill,
-    hill2,
-    lymrith,
-    storm2,
-    storm,
-    stone,
-    zephyros,
+    { src: loy2, className: "w-1/4 min-w-[70px] " },
+    { src: dreeco2, className: "w-1/5 min-w-[70px]  mr-[5%]" },
+    { src: climber2, className: "w-1/3 min-w-[70px] -mt-[15%]" },
+    { src: joia2, className: "w-1/3 min-w-[70px] -mt-[20%]" },
+    { src: sylas2, className: "w-1/3 min-w-[70px] -mt-[12%] -scale-x-100" },
+    { src: leon2, className: "w-1/4 min-w-[70px] -mt-[18%] mr-[5%]" },
+    { src: mk2, className: "w-1/3 min-w-[70px] -mt-[10%]" },
+    { src: verdan2, className: "w-1/3 min-w-[70px] -mt-[22%]" },
+    { src: hill, className: "w-[40%] min-w-[70px] " },
+    { src: hill2, className: "w-[40%] min-w-[70px] -scale-x-100" },
+    { src: stone2, className: "w-[60%] min-w-[70px] -mt-[40%]" },
+    { src: stone, className: "w-[45%] min-w-[70px] -mt-[30%]" },
+    { src: frost, className: "w-1/2 min-w-[70px] -mt-[30%]" },
+    { src: frost2, className: "w-[65%] min-w-[70px] -mt-[30%]" },
+    { src: fire, className: "w-[60%] min-w-[70px] -mt-[30%]" },
+    { src: fire2, className: "w-[40%] min-w-[70px] -mt-[25%] -scale-x-100" },
+    { src: cloud, className: "w-1/2 min-w-[70px] -mt-[20%]" },
+    { src: cloud2, className: "w-[60%] min-w-[70px] -mt-[23%] -scale-x-100" },
+    { src: storm2, className: "w-[40%] min-w-[70px] -mt-[30%]" },
+    { src: storm, className: "w-[60%] min-w-[70px] -mt-[30%] -scale-x-100" },
+    { src: zephyros, className: "w-1/2 min-w-[70px] -mt-[30%]" },
+    { src: harshnag, className: "w-[60%] min-w-[70px] -mt-[35%]" },
+    { src: hecaton, className: "w-[65%] min-w-[70px] -mt-[30%]" },
+    { src: lymrith, className: "w-1/2 min-w-[70px] -mt-[30%] -scale-x-100" },
   ];
 
   return (
-    <div className="h-[300vh] w-full bg-gradient-to-b via-[#2f4f4f] from-[#8b0000] to-[#191970] pt-[50px] flex flex-wrap">
+    <div className="h-auto w-full bg-gradient-to-b via-[#2f4f4f] from-[#8b0000] to-[#191970] pt-[50px] flex flex-wrap">
       {images.map((image, index) => (
         <div
           key={index}
           className={`w-1/2 p-2 ${
             index % 2 === 0 ? "justify-start" : "justify-end"
-          } flex`}
+          } flex trans`}
         >
           <img
-            src={image}
+            src={image.src}
             alt={`image-${index}`}
-            className="w-1/4 image-shadow object-contain "
+            className={`${image.className} image-shadow object-contain`}
           />
         </div>
       ))}
+
+      <div className="absolute w-full px-[20%] text-white flex flex-col justify-center items-center text-justify">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta?
+        </div>
+        <div className="px-[5%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          voluptatum cupiditate voluptate sit doloremque? Necessitatibus,
+          recusandae consequatur. Molestiae tenetur minus repellendus doloremque
+          tempora deleniti vitae dolorem omnis enim, ea perferendis aperiam
+          atque ab incidunt ad cum tempore quia facilis asperiores? Eligendi
+          dolore laudantium, modi quis quas necessitatibus possimus excepturi
+          soluta?
+        </div>
+      </div>
     </div>
   );
 }
